9446b4037927742e7b6dc965eb776fb966722126
Nitesh-ni30 <44554000+Nitesh-ni30@users.noreply.github.com>
2022-08-11 01:47:05 +0530

    Create data_types.cpp

---
+++ data_types.cpp
@@ -0,0 +1,26 @@
+// Lecture 2
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int a=123;
+    cout<< a <<endl;
+
+    char b='T';
+    cout<< b <<endl;
+
+    bool bl=false;
+    cout<< bl <<endl;
+    
+    float f=7.8645;
+    cout<< f <<endl;
+
+    double d=5.937632;
+    cout<< d <<endl;
+
+    int size= sizeof(d);
+    cout<< "size of double:: "<<size<<endl;
+
+    return 0;
+}
