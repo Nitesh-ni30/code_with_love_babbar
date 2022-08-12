b5957b525bc5774bdeb8145c666bc11d18eb521e
Nitesh-ni30 <44554000+Nitesh-ni30@users.noreply.github.com>
2022-08-12 22:14:08 +0530

    Create pattern_4.cpp

---
+++ pattern_4.cpp
@@ -0,0 +1,31 @@
+/*
+
+ *
+ * *
+ * * *
+ * * * *
+ * * * * *
+
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    int n;
+    cin>>n;
+
+    int row=1;
+    while (row<=n){
+
+        int col=1;
+        while(col<=row){
+           cout<<"*"<<" ";
+           col+=1;
+        }
+        cout<<endl;
+        row+=1;
+
+    }
+
+    return 0;
+}
