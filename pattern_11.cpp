7f5d9f8a850219f17023029fa43e880fc73cdc8e
Nitesh-ni30 <44554000+Nitesh-ni30@users.noreply.github.com>
2022-08-13 22:47:34 +0530

    Create pattern_11.cpp

---
+++ pattern_11.cpp
@@ -0,0 +1,36 @@
+/*
+
+A B C D E F G
+A B C D E F G
+A B C D E F G
+A B C D E F G
+A B C D E F G
+A B C D E F G
+A B C D E F G
+
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int row=1;
+    
+    while(row<=n)
+    {
+        int col=1;
+        
+        while(col<=n)
+        {
+            char ch='A'+col-1;
+            cout<<ch<<" ";
+            col+=1;
+        }
+        cout<<endl;
+        row+=1;
+    }
+    return 0;
+
+}
