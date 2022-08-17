ba9215fc154250060ef44778e05b47bac831b2f1
Nitesh-ni30 <44554000+Nitesh-ni30@users.noreply.github.com>
2022-08-17 23:31:40 +0530

    Create pre_post_operator.cpp

---
+++ pre_post_operator.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a=1,b=2;
+    if(a-->0 && ++b>2){
+        cout<<"inside if";
+    }
+    else{
+        cout<<"inside else";
+    }
+    cout<<a<<" "<<b<<endl;
+    return 0;
+}
