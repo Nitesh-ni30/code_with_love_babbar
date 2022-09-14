de77c2a1b32b9634aa517b35dff10f74787b6086
Nitesh-ni30 <44554000+Nitesh-ni30@users.noreply.github.com>
2022-09-15 04:00:23 +0530

    Create practice qs of pass by value.cpp

---
+++ practice qs of pass by value.cpp
@@ -0,0 +1,38 @@
+//practice of pass by value
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+void update(int a){
+    a=a/2;
+}
+int main(){
+    int a=10;
+    update(a);
+    cout<<a<<endl;
+    return 0;
+}
+*/
+// 2nd qs
+/*
+int update(int a){
+    a-=5;
+    return a;
+}
+int main(){
+    int a=15;
+    update(a);
+    cout<<a<<endl;
+    return 0;
+}
+*/
+int update(int a){
+    int ans=a*a;
+    return ans;
+}
+int main(){
+    int a=14;
+    a=update(a);
+    cout<<a<<endl;
+    return 0;
+}
